1fd774f9074d1dadfe87885735b9bedd8c1910e0
Richard Schneider <makaretu@gmail.com>
2016-09-11 14:54:40 +1200

    docs(messages): add message syntax

--- doc/make.js
+++ doc/make.js
@@ -2,6 +2,7 @@
 
 let tm = require('../index'),
     g = tm.grammar,
+    syntax = require('./syntax'),
     fs = require('fs'),
     mkdirp = require('mkdirp'),
     path = require('path'),
@@ -22,12 +23,18 @@ let messages = g.root
     .terms.map(rule => rule.name)
     .sort();
 messages = messages.map(name => {
-    var example, ast;
+    var example, ast, def;
     g.rules[name].example(g, (e,s) => { example = s; });
     tm.parse(example, (e,o) => { ast = JSON.stringify(o, null, 4); });
+    def = syntax.rule(name);
+    syntax.dependencies(name).forEach(r => {
+        def += '\n' + syntax.rule(r);
+    });
     return {
         name: name,
         example: example,
+        definition: def,
+        description: '',
         ast: ast
     };
 });
---
+++ doc/syntax.js
@@ -0,0 +1,85 @@
+'use strict';
+
+let tm = require('../index'),
+    grammar = tm.grammar;
+
+function termHtml(term) {
+    if('optionally' in term) {
+        return ' [' + termHtml(term.optionally) + ']';
+    }
+    else if('repeating' in term) {
+        return ' (' + termHtml(term.repeating) + ']) ...';
+    }
+    else if('keyword' in term) {
+        return ` <span class="keyword">${term.keyword}</span>`;
+    }
+    else if('other' in term) {
+        return ` <var class="rule-name">${term.other}</var>`;
+    }
+    else if('number' in term) {
+        return ` <span class="number">#</span>`;
+    }
+    else if('sequence' in term) {
+        return term.sequence.reduce((html, t) => html += termHtml(t), ' ');
+    }
+    else if('either' in term) {
+        return term.either.reduce((html, t) => html += termHtml(t) + ' |', ' ');
+    }
+}
+
+module.exports.rule = function rule(name) {
+    let html = `<var>${name}</var> :=`;
+
+    grammar.rules[name].dump(grammar, (e,t) => html += termHtml(t));
+
+    return html;
+};
+
+function pushOther(others, other) {
+    if (Array.isArray(other)) {
+        other.forEach(e => pushOther(others, e));
+    } else if (other !== null && others.indexOf(other) === -1) {
+        others.push(other);
+    }
+
+    return others;
+}
+
+function termOther(term) {
+    if('optionally' in term) {
+        return termOther(term.optionally);
+    }
+    else if('repeating' in term) {
+        return termOther(term.repeating);
+    }
+    else if('other' in term) {
+        return term.other;
+    }
+    else if('sequence' in term) {
+        return term.sequence.reduce((others, t) => pushOther(others, termOther(t)), []);
+    }
+    else if('either' in term) {
+        return term.either.reduce((others, t) => pushOther(others, termOther(t)), []);
+    }
+
+    return null;
+}
+
+module.exports.dependencies = function dependencies(name, others, visited) {
+    others = others || [];
+    visited = visited || [];
+
+    if (visited.indexOf(name) >= 0) {
+        return others;
+    }
+    visited.push(name);
+
+    grammar.rules[name].dump(grammar, (e,t) => pushOther(others, termOther(t)));
+
+    for (let i = 0; i < others.length; ++i) {
+        dependencies(others[i], others, visited);
+    }
+
+    return others;
+};
+
